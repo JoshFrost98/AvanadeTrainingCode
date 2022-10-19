9fc4ffa98443b469e4b5be5ee2bd9f1c1a4db264
JFrost1998 <josh.frost@avanade.com>
2022-10-19 16:47:43 +0100

    JF: added booleans.py file

---
+++ booleans.py
@@ -0,0 +1,7 @@
+num1 = float(input('Enter first number: '))
+num2 = float(input('Enter second number: '))
+if num1 > num2:
+    num1bigger = True
+else:
+    num1bigger= False
+print('It is ', num1bigger, ' that num1 is bigger')

d2e9258f970c9ca4dcab797c6d5b06d4997899ab
JFrost1998 <josh.frost@avanade.com>
2022-10-20 10:15:46 +0100

    JF: updated temperature guage code to have just right temp

--- conditionals/temperatureguage.py
+++ conditionals/temperatureguage.py
@@ -2,6 +2,7 @@ temperature = 40
 if (temperature > 30):
     print('too hot')
     print('aagh')
-    if (temperature > 50):
-        print('AAH')
-print('too cold')
+if (temperature < 0):
+    print('too cold')
+if (temperature > 0):
+    print('too cold')
